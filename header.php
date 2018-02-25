3e4f40a03658e6270f7cdd7dc4a1819d65499376
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2018-02-25 00:15:18 -0600

    add new files

--- header.php
+++ header.php
@@ -5,17 +5,31 @@
     <head>
       <meta charset="<?php bloginfo('charset'); ?>">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="theme-color" content="#6d9aea">
 
       <?php wp_head(); ?>
 
       <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+      <link rel="stylesheet" href="<?php get_template_directory_uri().'/assets/hamburgers/dist/hamburgers.min.css' ?>">
 
     </head>
 
     <body <?php body_class(); ?>>
 
         <header class="main_header">
+
+            <?php
+              if ( function_exists( 'the_custom_logo' ) ):
+                  the_custom_logo();
+              endif;
+            ?>
+
+            <button class="hamburger hamburger--squeeze" type="button">
+              <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+              </span>
+            </button>
+
             <nav role="navigation">
 
             </nav>
--- index.php
+++ index.php
@@ -12,37 +12,8 @@
 
     <main class="page_container">
 
-      <div class="hero">
-        <h1>HOW TO MAINTAIN A HAPPY STREAK?</h1>
-
-        <div class="hero__overlay"></div>
-        <figure style="background-image: url(<?php echo get_template_directory_uri() . '/img/temp/hero.jpg'; ?>);">
-        </figure>
-      </div>
-
-      <section class="blog-listing">
-
-        <article class="blog-item">
-          <div class="item_overlay"></div>
-          <img src="<?php echo get_template_directory_uri() . '/img/temp/post1.jpg' ?>" alt="">
-
-        </article>
-
-        <article class="blog-item">
-          <div class="item_overlay"></div>
-          <img src="<?php echo get_template_directory_uri() . '/img/temp/post2.jpg' ?>" alt="">
-
-        </article>
-
-      </section>
-
-
-
 
     </main>
-
-
-
 
 
 <?php
